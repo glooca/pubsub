dcc749917996e4306bddb694d2d87565b12535c1
kSyri <kasper.syri@gmail.com>
2020-10-30 16:34:52 +0200

    Add vscode settings.json

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const PubSub = {
-    callbacks: {},
-    subscribe(ev, callback) {
-        if (this.callbacks[ev] && this.callbacks[ev].includes(callback)) return this;
-        (this.callbacks[ev] || (this.callbacks[ev] = [])).push(callback);
-        return this;
-    },
-    unsubscribe(ev, callback) {
-        if (!this.callbacks[ev]) return this;
-        this.callbacks[ev] = this.callbacks[ev].filter(item => item !== callback)
-        return this;
-    },
-    publish(ev, ...args) {
-        this.callbacks[ev].forEach(callback => callback.apply(this, args));
-        return this;
-    }
+  callbacks: {},
+  subscribe(ev, callback) {
+    if (this.callbacks[ev] && this.callbacks[ev].includes(callback)) return this;
+    (this.callbacks[ev] || (this.callbacks[ev] = [])).push(callback);
+    return this;
+  },
+  unsubscribe(ev, callback) {
+    if (!this.callbacks[ev]) return this;
+    this.callbacks[ev] = this.callbacks[ev].filter(item => item !== callback)
+    return this;
+  },
+  publish(ev, ...args) {
+    this.callbacks[ev].forEach(callback => callback.apply(this, args));
+    return this;
+  }
 }
 module.exports = PubSub;
