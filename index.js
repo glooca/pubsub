3e8705e99d69f1927c0e96cedea19b83730fe241
kSyri <kasper.syri@gmail.com>
2020-10-31 02:32:42 +0200

    Use constructor function

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const PubSub = {
-  callbacks: {},
-  subscribe(ev, callback) {
-    if (this.callbacks[ev] && this.callbacks[ev].includes(callback)) return this;
-    (this.callbacks[ev] || (this.callbacks[ev] = [])).push(callback);
-    return this;
-  },
-  unsubscribe(ev, callback) {
-    if (!this.callbacks[ev]) return this;
-    this.callbacks[ev] = this.callbacks[ev].filter(item => item !== callback)
-    return this;
-  },
-  publish(ev, ...args) {
-    this.callbacks[ev].forEach(callback => callback.apply(this, args));
-    return this;
-  }
+function PubSub() {
+  this.callbacks = {};
+}
+PubSub.prototype.subscribe = function(ev, callback) {
+  if (this.callbacks[ev] && this.callbacks[ev].includes(callback)) return this;
+  (this.callbacks[ev] || (this.callbacks[ev] = [])).push(callback);
+  return this;
+}
+PubSub.prototype.unsubscribe = function(ev, callback) {
+  if (!this.callbacks[ev]) return this;
+  this.callbacks[ev] = this.callbacks[ev].filter(item => item !== callback)
+  return this;
+}
+PubSub.prototype.publish = function(ev, ...args) {
+  if (!this.callbacks[ev]) return this;
+  this.callbacks[ev].forEach(callback => callback.apply(this, args));
+  return this;
 }
 module.exports = PubSub;
